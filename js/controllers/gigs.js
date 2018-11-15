343b61bb055b99a7c1df0e308153f7b17cdcd429
Chris Spencer <chris@chrisspencercreative.com>
2018-11-15 09:14:35 -0800

    initial launch of site rebuild

---
+++ js/controllers/gigs.js
@@ -0,0 +1,13 @@
+myApp.controller('GigsController', ['$scope', function($scope) {
+        //update google analytics
+    var url = window.location.href;
+    gtag('config', 'UA-20609405-4', {
+        'page_path' : '/#/gigs',
+        'page_location' : url,
+        'page_title' : 'Gigs'
+    
+    });
+    $('iframe').load(function() {
+        console.log('Iframe loaded');
+    });//iframe loaded
+}]);//controller
